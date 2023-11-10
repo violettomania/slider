6adf558cef6f13f6b885c17f17fac7874eeb73dd
violettomania <danielpolya@protonmail.com>
2023-11-10 12:42:41 +0100

    feature: display the first person's card in the card list

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
+import { useState } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 
+import people from './data';
+
+interface Props {
+  id: number;
+  image: string;
+  name: string;
+  title: string;
+  quote: string;
+}
+
 export default function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
-    <section className='section'>
-      <article style={{ transform: 'translateX(0%); opacity: 1;' }}>
-        <img
-          src='https://www.course-api.com/images/people/person-1.jpeg'
-          alt='maria ferguson'
-          className='person-img'
-        ></img>
-        <h5>maria ferguson</h5>
-        <p className='title'>office manager</p>
-        <p className='text'>
-          Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid
-          viral kombucha gentrify fanny pack raclette pok pok mustache.
-        </p>
-        <FaQuoteRight className='icon' />
-      </article>
-      <button className='prev'>
-        <FiChevronLeft />
-      </button>
-      <button className='next'>
-        <FiChevronRight />
-      </button>
-    </section>
+    <main>
+      <section className='section'>
+        {people.map((person: Props, index) => {
+          if (index === selectedIndex) {
+            const { id, image, name, title, quote } = person;
+            return (
+              <article key={id} className=''>
+                <img src={image} alt={name} className='person-img' />
+                <h4>{name}</h4>
+                <p className='title'>{title}</p>
+                <p className='text'>{quote}</p>
+                <FaQuoteRight className='icon' />
+              </article>
+            );
+          }
+        })}
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
+      </section>
+    </main>
   );
 }
