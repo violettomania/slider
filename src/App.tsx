2ba96e1ea420791126110099cb20155e1890503b
violettomania <danielpolya@protonmail.com>
2023-11-10 14:01:32 +0100

    feature: autoscroll stops when user clicks on button, then restarts after inactivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useRef } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
 import people from './data';
@@ -6,26 +6,48 @@ import Person from './components/Person';
 
 export default function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const autoScrollInterval = useRef<NodeJS.Timeout | null>(null);
+  const restartTimeout = useRef<NodeJS.Timeout | null>(null);
 
-  const handleLeftClick = useCallback(() => {
-    setSelectedIndex((prevIndex) =>
-      prevIndex === 0 ? people.length - 1 : prevIndex - 1
-    );
-  }, []);
-
-  const handleRightClick = useCallback(() => {
+  const updateIndex = useCallback(() => {
     setSelectedIndex((prevIndex) =>
       prevIndex === people.length - 1 ? 0 : prevIndex + 1
     );
   }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      handleRightClick();
+  /*
+    The Person components are scrolled right automatically every 3 seconds.
+    If the user clicks on the left or right button, the auto scroll is stopped, and restarted after 6 seconds of inactivity.
+  */
+  const startAutoScroll = useCallback(() => {
+    autoScrollInterval.current = setInterval(() => {
+      updateIndex();
     }, 3000);
+  }, [updateIndex]);
 
-    return () => clearInterval(interval);
-  }, [handleRightClick]);
+  const handleRightClick = useCallback(() => {
+    updateIndex();
+    if (autoScrollInterval.current) clearInterval(autoScrollInterval.current);
+    if (restartTimeout.current) clearTimeout(restartTimeout.current);
+    restartTimeout.current = setTimeout(startAutoScroll, 6000);
+  }, [startAutoScroll, updateIndex]);
+
+  useEffect(() => {
+    startAutoScroll();
+    return () => {
+      if (autoScrollInterval.current) clearInterval(autoScrollInterval.current);
+      if (restartTimeout.current) clearTimeout(restartTimeout.current);
+    };
+  }, [startAutoScroll]);
+
+  const handleLeftClick = useCallback(() => {
+    setSelectedIndex((prevIndex) =>
+      prevIndex === 0 ? people.length - 1 : prevIndex - 1
+    );
+    if (autoScrollInterval.current) clearInterval(autoScrollInterval.current);
+    if (restartTimeout.current) clearTimeout(restartTimeout.current);
+    restartTimeout.current = setTimeout(startAutoScroll, 6000);
+  }, [startAutoScroll]);
 
   return (
     <main>
